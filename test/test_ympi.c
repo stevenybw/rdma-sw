1e2a3cb30985220150ee6dea27f312cb8eb83fca
Bowen Yu <stevenybw@hotmail.com>
2017-06-29 16:43:09 +0800

    inc

--- test/test_ympi.c
+++ test/test_ympi.c
@@ -7,39 +7,92 @@
 #include "ympi.h"
 
 int main(void) {
-	int i, rank, nprocs;
-	YMPI_Init(NULL, NULL);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-	LOGDS("Testing YMPI...\n");
+  int i, rank, nprocs;
+  int iters = 1024;
+  YMPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  assert(nprocs == 2);
 
-	if(rank == 0) {
-		YMPI_Rdma_buffer send_buffer;
-		uint64_t* sb = NULL;
-                uintptr_t sb_ptr = 0;
-		YMPI_Alloc(&send_buffer, 1024);
-		YMPI_Get_buffer(send_buffer, &sb_ptr);
-                sb = (uint64_t*) sb_ptr;
-		assert(sb != NULL);
-		for(i=0; i<128; i++) {
-			sb[i] = i;
-		}
-		YMPI_Post_send(send_buffer, 0, 1024, 1);
-		YMPI_Expect(1, 0, NULL, NULL);
-		YMPI_Dealloc(&send_buffer);
-	} else if(rank == 1) {
-		uint64_t* recv_buffer = NULL;
-		uint64_t  recv_buffer_len = 0;
-		YMPI_Expect(0, 1, &recv_buffer, &recv_buffer_len);
-                printf("recv_buffer     = %p\n", recv_buffer);
-                printf("recv_buffer_len = %d\n", recv_buffer_len);
-		assert(recv_buffer_len == 1024);
-		for(i=0; i<128; i++) {
-			assert(recv_buffer[i] == i);
-		}
-	}
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
 
-	YMPI_Finalize();
-	return 0;
+  LOGDS("Testing point-to-point correctness (%d packet)...\n", iters);
+  if(rank == 0) {
+    YMPI_Rdma_buffer send_buffer;
+    uint64_t* sb = NULL;
+        uintptr_t sb_ptr = 0;
+    YMPI_Alloc(&send_buffer, 1024);
+    YMPI_Get_buffer(send_buffer, &sb_ptr);
+    sb = (uint64_t*) sb_ptr;
+    assert(sb != NULL);
+    for(i=0; i<YMPI_PREPOST_DEPTH; i++) {
+      sb[i] = 0x1111111111111111 + i;
+      YMPI_Post_send(send_buffer, i * sizeof(uint64_t), sizeof(uint64_t), 1);
+    }
+    YMPI_Expect(YMPI_PREPOST_DEPTH, 0, NULL, NULL);
+    YMPI_Dealloc(&send_buffer);
+  } else if(rank == 1) {
+    uint64_t* recv_buffers[YMPI_PREPOST_DEPTH];
+    uint64_t  recv_buffers_len[YMPI_PREPOST_DEPTH];
+    memset(recv_buffers, 0, sizeof(recv_buffers));
+    memset(recv_buffers_len, 0, sizeof(recv_buffers_len));
+    YMPI_Expect(0, YMPI_PREPOST_DEPTH, recv_buffers, recv_buffers_len);
+
+    printf("recv_buffer   = %p\n", recv_buffer);
+    printf("recv_buffer_len = %d\n", recv_buffer_len);
+    assert(recv_buffer_len == 1024);
+    for(i=0; i<YMPI_PREPOST_DEPTH; i++) {
+      printf("recv_buffers_len[%d]  = %d\n", i, recv_buffers_len[i]);
+      assert(recv_buffers_len[i] == sizeof(uint64_t));
+      printf("recv_buffers[%d]    = %p (*%llu)\n", i, recv_buffers[i], *(recv_buffers[i]));
+      assert(*(recv_buffer[i]) == i);
+    }
+    YMPI_Return();
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  LOGDS("Benchmarking message rate (%d iters, %d mesg/iter, tx_depth=%d)...\n", iters, YMPI_PREPOST_DEPTH, YMPI_PREPOST_DEPTH);
+
+  double wsec;
+  int k;
+  if(rank == 0) {
+    YMPI_Rdma_buffer send_buffer;
+    uint64_t* sb = NULL;
+    uintptr_t sb_ptr = 0;
+    YMPI_Alloc(&send_buffer, 1024);
+    YMPI_Get_buffer(send_buffer, &sb_ptr);
+    sb = (uint64_t*) sb_ptr;
+    assert(sb != NULL);
+
+    wsec = -MPI_Wtime();
+    for(k=0; k<iters; k++) {
+      for(i=0; i<YMPI_PREPOST_DEPTH; i++) {
+        sb[i] = 0x1111111111111111 + i;
+        YMPI_Post_send(send_buffer, i * sizeof(uint64_t), sizeof(uint64_t), 1);
+      }
+      YMPI_Expect(YMPI_PREPOST_DEPTH, 0, NULL, NULL);
+    }
+    wsec += MPI_Wtime();
+
+    YMPI_Dealloc(&send_buffer);
+  } else if(rank == 1) {
+    uint64_t* recv_buffers[YMPI_PREPOST_DEPTH];
+    uint64_t  recv_buffers_len[YMPI_PREPOST_DEPTH];
+    memset(recv_buffers, 0, sizeof(recv_buffers));
+    memset(recv_buffers_len, 0, sizeof(recv_buffers_len));
+
+    wsec = -MPI_Wtime();
+    for(k=0; k<iters; k++) {
+      YMPI_Expect(0, YMPI_PREPOST_DEPTH, recv_buffers, recv_buffers_len);
+      YMPI_Return();
+    }
+    wsec += MPI_Wtime();
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  LOGD("time=%lf us,  msg_rate=%lf Mmesg/s\n", wsec, 1e-6 * iters * YMPI_PREPOST_DEPTH / wsec);
+
+  YMPI_Finalize();
+  return 0;
 }
-
